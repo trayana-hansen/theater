78bacb327da047d78b4e93c8071d15a80e67e663
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-09-07 22:33:31 +0200

    login basic setup

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./Components/Styles/Theme";
+import { AuthProvider } from "./Components/App/Auth/Auth.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
